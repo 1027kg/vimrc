d41277aae037719d689a95c2afcf895f54577e13
1027kg <technocuters@gmail.com>
2021-05-05 17:03:32 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,11 +8,11 @@ mv molokai/colors/molokai.vim ~/.vim/colors/
 
 cp ./vimprofile ~/.vimrc
 
-echo "alias cp='cp -i'" >> ~/.bash_profile
-echo "alias mv='mv -i'" >> ~/.bash_profile
-echo "alias rm='rm -i'" >> ~/.bash_profile
-echo "alias ll='ls -lA'" >> ~/.bash_profile
-echo "alias la='ls -a'" >> ~/.bash_profile
+echo "alias cp='cp -i'" >> ~/.bashrc
+echo "alias mv='mv -i'" >> ~/.bashrc
+echo "alias rm='rm -i'" >> ~/.bashrc
+echo "alias ll='ls -lA'" >> ~/.bashrc
+echo "alias la='ls -a'" >> ~/.bashrc
 
 mkdir ~/Desktop/nodedocs
 mkdir ~/Desktop/pydocs
