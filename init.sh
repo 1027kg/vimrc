df18004edeab43fbc1cda5102007897e9a9d4e0e
1027kg <technocuters@gmail.com>
2021-05-05 17:46:54 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
-sudo apt-get install vim
+#! /bin/bash
+
+sudo apt-get install vim git
 
 mkdir ~/.vim
 mkdir ~/.vim/colors
 
 git clone https://github.com/tomasr/molokai
 mv molokai/colors/molokai.vim ~/.vim/colors/
-
 cp ./vimprofile ~/.vimrc
 
 echo "alias cp='cp -i'" >> ~/.bashrc
@@ -17,6 +18,12 @@ echo "alias la='ls -a'" >> ~/.bashrc
 mkdir ~/Desktop/nodedocs
 mkdir ~/Desktop/pydocs
 
-# sudo swapoff --all
-# sudo systemctl stop dphys-swapfile
-# sudo systemctl disable dphys-swapfile
+sudo swapoff --all
+sudo systemctl stop dphys-swapfile
+sudo systemctl disable dphys-swapfile
+
+# change hostname
+# echo "No.1=" $1
+# cp /etc/hostname /etc/hostname.backup
+# cp /etc/hosts /etc/hosts.backup
+# echo "" >> /etc/hostname
